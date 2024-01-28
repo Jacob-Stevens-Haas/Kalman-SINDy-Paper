f2657717670f88c38102dc3632bf4904dd146cb3
Jake <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2024-01-28 17:03:00 +0000

    Extract to plotting functions

--- images/gen_image/ksindy_figs/__init__.py
+++ images/gen_image/ksindy_figs/__init__.py
@@ -1,23 +1,39 @@
 # %%
+from typing import Any
+from typing import cast
+from typing import Callable
 from typing import NewType
+from typing import Optional
+from typing import TypeVar
 
-import kalman
+import kalman  # type: ignore
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import sparse
+from numpy.typing import NBitBase
+from scipy import sparse  # type: ignore
 from sklearn.linear_model import Lasso
 
+NpFlt = np.dtype[np.floating[NBitBase]]
 NTimesteps = NewType("NTimesteps", int)
 NSeries = NewType("NSeries", int)
-Float1D = np.ndarray[NTimesteps, np.dtype[np.floating]]
+Float1D = np.ndarray[NTimesteps, NpFlt]
+Float2D = np.ndarray[tuple[NSeries, NTimesteps], NpFlt]
+FloatOrArray = TypeVar("FloatOrArray", float, NpFlt)
 
 CMAP = mpl.color_sequences['tab10']
+CTRUE = CMAP[0]
+CMEAS = CMAP[1]
+CEST = CMAP[2]
+
 np.random.seed(1)
 NOISE_VAR = .2
 
-f_dot = lambda t: .3 * t **2 - 2 * t
-f = lambda t: 20 / (3 + 2 * np.e ** (2 * t))
+def f_dot(t: FloatOrArray) -> FloatOrArray:
+    return .3 * t **2 - 2 * t  # type: ignore 
+
+def f(t: FloatOrArray) -> FloatOrArray:
+    return 20 / (3 + 2 * np.e ** (2 * t)) # type: ignore
 
 def generate_mock_data(dt: float, noise_var: float) -> tuple[float, Float1D, Float1D, Float1D, Float1D]:
     t = np.arange(0, 6, dt)
@@ -27,7 +43,7 @@ def generate_mock_data(dt: float, noise_var: float) -> tuple[float, Float1D, Flo
     z = x + np.random.normal(size=x.shape, scale=np.sqrt(noise_var))
     return nt, t, x, x_dot, z
 def alt_mock_data(dt: float, noise_var: float) -> tuple[float, Float1D, Float1D, Float1D, Float1D]:
-    t = np.arange(0, 6, dt)
+    t = np.arange(0, 6, dt, dtype=float)
     nt = len(t)
     x = f(t)
     x_dot = f_dot(t)
@@ -35,7 +51,7 @@ def alt_mock_data(dt: float, noise_var: float) -> tuple[float, Float1D, Float1D,
     return nt, t, x, x_dot, z
 
 # %%
-nt, t, x, x_dot, z = generate_mock_data(.3, NOISE_VAR)
+# nt, t, x, x_dot, z = generate_mock_data(.3, NOISE_VAR)
 nt, t, x, x_dot, z = alt_mock_data(.3, NOISE_VAR)
 # %%
 zoom_start = len(t) //3
@@ -50,45 +66,114 @@ x_hat, x_dot_hat, G, Qinv, loss = kalman.solve(z, H, t, NOISE_VAR, 1e-1)
 
 # %%
 
-plt.plot(t, x, label="true")
-plt.plot(t, z, ".", label="measured")
-plt.plot(t, x_hat, label="kalman")
-plt.legend()
 
 # %%
 def fd_arrows(
     x: Float1D,
     y: Float1D
-) -> np.ndarray[tuple[NTimesteps, NSeries], np.dtype[np.floating]]:
+) -> np.ndarray[tuple[NSeries, NTimesteps], np.dtype[np.floating]]:
     dx = x[1:] - x[:-1]
     dy = y[1:] - y[:-1]
     return np.stack((dx, dy), axis=0)
 
 # %%
-fd = fd_arrows(t, x)[:, zoom_inds]
-zd = fd_arrows(t, z)[:, zoom_inds]
+fd = fd_arrows(t, x)
+zd = fd_arrows(t, z)
+
+q_props = {"headwidth": 1.5, "headlength": 2, "angles": "xy"}
+
+# %%
+def data_plot(
+    t: Float1D,
+    x: Float1D,
+    z: Float1D,
+    *,
+    ylims: Optional[tuple[float, float]] = None
+) -> None:
+    ax = plt.figure().add_axes((0,0,1,1))
+    ax.plot(t, x, color=CTRUE, label="true")
+    ax.plot(t, z, ".", color=CMEAS, label="measured")
+    ax.legend()
+    if ylims is not None:
+        ax.set_ylim(*ylims)
+
 
 
 # %%
-q_props = {"headwidth": 1.5, "headlength": 2, }
-# plt.plot(t[zoom_inds], x[zoom_inds], label="true")
-plt.title("Kalman smoothing applied to measurements")
-plt.plot(t[zoom_inds], z[zoom_inds], ".", color=CMAP[0], label="measured")
-plt.plot(t[zoom_inds], x_hat[zoom_inds], "--", color=CMAP[1], label="kalman")
-plt.quiver(t[zoom_inds], z[zoom_inds], zd[0], zd[1], color=CMAP[0], **q_props)
-plt.quiver(t[zoom_inds], x_hat[zoom_inds], zd[0], x_dot_hat[zoom_inds]*zd[0], color=CMAP[1], **q_props)
-# plt.plot(t[zoom_inds], x_dot_hat[zoom_inds], color=cmap[1])
-plt.legend()
+def smoothing_plot(
+    t: Float1D,
+    z: Float1D,
+    zd: Float2D,
+    x_hat: Float1D,
+    x_dot_hat: Float1D,
+    *,
+    ylims: Optional[tuple[float, float]] = None,
+    **q_props: Any
+) -> None:
+    dt = zd[0]
+    ax = plt.figure().add_axes((0,0,1,1))
+    ax.set_title("Kalman smoothing applied to measurements")
+    ax.plot(t, z, ".", color=CMEAS, label="measured")
+    ax.plot(t, x_hat, "--", color=CEST, label="kalman")
+    ax.quiver(t, z, dt, zd[1], color=CMEAS, **q_props)
+    ax.quiver(t, x_hat, dt, x_dot_hat * dt, color=CEST, **q_props)
+    ax.legend()
+    if ylims is not None:
+        ax.set_ylim(*ylims)
+
+
 
 # %%
-funcs_theta = np.stack((np.ones_like(x_hat), x_hat, x_hat**2), axis=0)
-xyu = (t[zoom_inds], x_hat[zoom_inds], zd[0])
-plt.plot(t[zoom_inds], x_hat[zoom_inds], ".", color=CMAP[1], label="kalman")
-plt.quiver(*xyu, x_dot_hat[zoom_inds]*zd[0], color=CMAP[1])
-plt.quiver(*xyu, funcs_theta[0, zoom_inds]*zd[0], **q_props)
-plt.quiver(*xyu, funcs_theta[1, zoom_inds]*zd[0], **q_props)
-plt.quiver(*xyu, funcs_theta[2, zoom_inds]*zd[0], **q_props)
-# %%
+def lib_plot(
+    t: Float1D,
+    dt: Float1D,
+    x_hat: Float1D,
+    x_dot_hat: Float1D,
+    funcs_theta: Float2D,
+    ylims: Optional[tuple[float, float]] = None,
+    **q_props: Any
+) -> None:
+    """Create plot of function library vectors and smoothed derivative vectors."""
+    ax = plt.figure().add_axes((0,0,1,1))
+    ax.plot(t, x_hat, ".", color=CEST, label="Kalman")
+    ax.quiver(t, x_hat, dt, x_dot_hat * dt, color=CEST, **q_props)
+    for i, func in enumerate(funcs_theta):
+        ax.quiver(t, x_hat, dt, func * dt, color=CMAP[i+3], label=f"$θ_{i}$", **q_props)
+    if ylims is not None:
+        ax.set_ylim(*ylims)
+    ax.legend()
 
-model = Lasso(1e-1, fit_intercept=False).fit(funcs_theta.T, x_dot_hat)
-model.coef_
+
+# %%
+def shared_ylim(*args: Float1D) -> tuple[float, float]:
+    data_lim = (cast(float, np.min(args)), cast(float, np.max(args)))
+    diff = data_lim[1] - data_lim[0]
+    margin = diff * .1
+    return data_lim[0] - margin, data_lim[1] + margin
+
+# %%
+if __name__ == "__main__":
+    ylims = shared_ylim(x[zoom_inds], z[zoom_inds], x_hat[zoom_inds])
+    data_plot(t[zoom_inds], x[zoom_inds], z[zoom_inds], ylims=ylims)
+    funcs_theta = np.stack((np.ones_like(x_hat), x_hat, x_hat**2), axis=0)
+    smoothing_plot(
+        t[zoom_inds],
+        z[zoom_inds],
+        zd[:, zoom_inds],
+        x_hat[zoom_inds],
+        x_dot_hat[zoom_inds],
+        ylims=ylims,
+        **q_props
+    )
+    lib_plot(
+        t[zoom_inds],
+        fd[0, zoom_inds],
+        x_hat[zoom_inds],
+        x_dot_hat[zoom_inds],
+        funcs_theta[:, zoom_inds],
+        ylims=ylims,
+        **q_props
+    )
+
+    model = Lasso(1e-1, fit_intercept=False).fit(funcs_theta.T, x_dot_hat)
+    model.coef_
