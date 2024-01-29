52dfbf2997beb2132ca8ec95e3094eaed87a3b46
Jake <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2024-01-29 15:33:21 +0000

    cln(gen_image): Extract funcs

--- images/gen_image/ksindy_figs/__init__.py
+++ images/gen_image/ksindy_figs/__init__.py
@@ -1,9 +1,11 @@
 # %%
+from dataclasses import dataclass
 from typing import Any
 from typing import cast
 from typing import Callable
 from typing import NewType
 from typing import Optional
+from typing import TypedDict
 from typing import TypeVar
 
 import kalman  # type: ignore
@@ -23,34 +25,60 @@ Float2D = np.ndarray[tuple[NSeries, NTimesteps], NpFlt]
 FloatND = np.ndarray[Any, NpFlt]
 FloatOrArray = TypeVar("FloatOrArray", float, FloatND)
 
-CMAP = mpl.color_sequences['tab10']
+
+@dataclass
+class PlotData:
+    """Container for 1-D plottable data"""
+
+    t: Float1D
+    dt: Float1D
+    x: Float1D
+    dx: Float1D
+    x_dot: Float1D
+    z: Float1D
+    dz: Float1D
+    x_hat: Float1D
+    x_dot_hat: Float1D
+    theta: Float2D
+
+
+CMAP = mpl.color_sequences["tab10"]
 CTRUE = CMAP[0]
 CMEAS = CMAP[1]
 CEST = CMAP[2]
 
 np.random.seed(1)
-NOISE_VAR = .2
+NOISE_VAR = 0.2
 
 # def f_dot(x: FloatOrArray) -> FloatOrArray:
-#     return x ** 2 - 2 * x  # type: ignore 
+#     return x ** 2 - 2 * x  # type: ignore
 
 # def f(t: FloatOrArray) -> FloatOrArray:
 #     return 2 / (1 + np.e ** (2 * t)) # type: ignore
 
+
 def f_dot(x: FloatOrArray) -> FloatOrArray:
-    return -4 -  3 * x  # type: ignore 
+    return -4 - 3 * x  # type: ignore
+
 
 def f(t: FloatOrArray) -> FloatOrArray:
-    return 3* np.e ** (-3 * t) - 4 /3# type: ignore
+    return 3 * np.e ** (-3 * t) - 4 / 3  # type: ignore
 
-def generate_sin_data(dt: float, noise_var: float) -> tuple[float, Float1D, Float1D, Float1D, Float1D]:
+
+def gen_sin_data(
+    dt: float, noise_var: float
+) -> tuple[float, Float1D, Float2D, Float2D, Float2D]:
     t = np.arange(0, 6, dt)
     nt = len(t)
     x = np.stack([np.sin(t), np.cos(t)], axis=0)
     x_dot = np.stack([np.cos(t), -np.sin(t)])
     z = x + np.random.normal(size=x.shape, scale=np.sqrt(noise_var))
     return nt, t, x, x_dot, z
-def gen_exp_data(dt: float, noise_var: float) -> tuple[float, Float1D, Float1D, Float1D, Float1D]:
+
+
+def gen_exp_data(
+    dt: float, noise_var: float
+) -> tuple[float, Float1D, Float1D, Float1D, Float1D]:
     t = np.arange(0, 1, dt, dtype=float)
     nt = len(t)
     x = f(t)
@@ -58,46 +86,101 @@ def gen_exp_data(dt: float, noise_var: float) -> tuple[float, Float1D, Float1D, 
     z = x + np.random.normal(size=x.shape, scale=np.sqrt(noise_var))
     return nt, t, x, x_dot, z
 
-# %%
-# nt, t, x, x_dot, z = generate_mock_data(.3, NOISE_VAR)
-nt, t, x, x_dot, z = gen_exp_data(.05, NOISE_VAR)
-# %%
-zoom_start = len(t) //3
-zoom_end = zoom_start + 8
-zoom_inds = slice(zoom_start, zoom_end, 1)
-plt.plot(t[zoom_inds], z[zoom_inds], ".")
 
-# %%
-H = sparse.lil_matrix((nt, 2 * nt))
-H[:, 1::2] = sparse.eye(nt)
-
-x_hat, x_dot_hat, G, Qinv, loss = kalman.solve(z, H, t, NOISE_VAR, 1e-1)
+class MathConfig(TypedDict):
+    ode_type: str
+    dt: float
+    t_end: float
 
 
-# %%
+class PlotConfig(TypedDict):
+    zoom_inds: slice
+    q_props: dict[str, Any]
+
+
+def run_example(m_conf: MathConfig, p_conf: PlotConfig) -> None:
+    t, x, x_dot, z, x_hat, x_dot_hat = gen_and_solve(m_conf)
+    zoom = p_conf["zoom_inds"]
+    dt = fd_arrows(t.reshape((1, -1)))
+    dx = fd_arrows(x)
+    dz = fd_arrows(z)
+
+    funcs_theta = ps.PolynomialLibrary(2).fit_transform(x_hat.T).T
+
+    pdat = PlotData(
+        t[zoom],
+        dt[0, zoom],
+        x[0, zoom],
+        dx[0, zoom],
+        x_dot[0, zoom],
+        z[0, zoom],
+        dz[0, zoom],
+        x_hat[0, zoom],
+        x_dot_hat[0, zoom],
+        funcs_theta[:, zoom],
+    )
+    make_all_plots(pdat)
+    model = ps.STLSQ(unbias=True).fit(funcs_theta.T, x_dot_hat.T)
+    print(model.coef_)
+
+
+def make_all_plots(pdat: PlotData) -> None:
+    ylims = shared_ylim(pdat.x, pdat.z, pdat.x_hat)
+    data_plot(pdat.t, pdat.x, pdat.z, ylims=ylims)
+    smoothing_plot(
+        pdat.t,
+        pdat.z,
+        pdat.dt,
+        pdat.dz,
+        pdat.x_hat,
+        pdat.x_dot_hat,
+        ylims=ylims,
+        **q_props,
+    )
+    lib_plot(
+        pdat.t, pdat.dt, pdat.x_hat, pdat.x_dot_hat, pdat.theta, ylims=ylims, **q_props
+    )
+
+
+def gen_and_solve(
+    config: MathConfig,
+) -> tuple[Float1D, Float2D, Float2D, Float2D, Float2D, Float2D]:
+    if config["ode_type"] == "sin":
+        nt, t, x, x_dot, z = gen_sin_data(0.3, NOISE_VAR)
+    else:
+        nt, t, x, x_dot, z = gen_exp_data(0.05, NOISE_VAR)
+        x = cast(Float2D, x.reshape((1, -1)))
+        x_dot = cast(Float2D, x_dot.reshape((1, -1)))
+        z = cast(Float2D, z.reshape((1, -1)))
+    H = sparse.lil_matrix((nt, 2 * nt))
+    H[:, 1::2] = sparse.eye(nt)
+    x_hats: list[Float1D] = []
+    x_dot_hats: list[Float1D] = []
+    for row_z in z:
+        x_hat, x_dot_hat, _, _, _ = kalman.solve(row_z, H, t, NOISE_VAR, 1e-1)  # type: ignore
+        x_hats.append(cast(Float1D, x_hat))
+        x_dot_hats.append(cast(Float1D, x_dot_hat))
+    x_hat = np.stack(x_hats)
+    x_dot_hat = np.stack(x_dot_hats)
+
+    return t, x, x_dot, z, cast(Float2D, x_hat), cast(Float2D, x_dot_hat)
+
+
 def fd_arrows(
-    x: Float1D,
-    y: Float1D
-) -> np.ndarray[tuple[NSeries, NTimesteps], np.dtype[np.floating]]:
-    dx = x[1:] - x[:-1]
-    dy = y[1:] - y[:-1]
-    return np.stack((dx, dy), axis=0)
+    x: Float2D,
+) -> Float2D:
+    deltas = [row[1:] - row[:-1] for row in x]
+    return np.array(deltas)
+
 
 # %%
-fd = fd_arrows(t, x)
-zd = fd_arrows(t, z)
 
-q_props = {"headwidth": 1.5, "headlength": 2, "angles": "xy"}
 
 # %%
 def data_plot(
-    t: Float1D,
-    x: Float1D,
-    z: Float1D,
-    *,
-    ylims: Optional[tuple[float, float]] = None
+    t: Float1D, x: Float1D, z: Float1D, *, ylims: Optional[tuple[float, float]] = None
 ) -> None:
-    ax = plt.figure().add_axes((0,0,1,1))
+    ax = plt.figure().add_axes((0, 0, 1, 1))
     ax.plot(t, x, color=CTRUE, label="true")
     ax.plot(t, z, ".", color=CMEAS, label="measured")
     ax.legend()
@@ -105,29 +188,27 @@ def data_plot(
         ax.set_ylim(*ylims)
 
 
-
 # %%
 def smoothing_plot(
     t: Float1D,
     z: Float1D,
-    zd: Float2D,
+    dt: Float1D,
+    dz: Float1D,
     x_hat: Float1D,
     x_dot_hat: Float1D,
     *,
     ylims: Optional[tuple[float, float]] = None,
-    **q_props: Any
+    **q_props: Any,
 ) -> None:
-    dt = zd[0]
-    ax = plt.figure().add_axes((0,0,1,1))
+    ax = plt.figure().add_axes((0, 0, 1, 1))
     ax.set_title("Kalman smoothing applied to measurements")
     ax.plot(t, z, ".", color=CMEAS, label="measured")
     ax.plot(t, x_hat, "--", color=CEST, label="kalman")
-    ax.quiver(t, z, dt, zd[1], color=CMEAS, **q_props)
+    ax.quiver(t, z, dt, dz, color=CMEAS, **q_props)
     ax.quiver(t, x_hat, dt, x_dot_hat * dt, color=CEST, **q_props)
     ax.legend()
     if ylims is not None:
         ax.set_ylim(*ylims)
-
 
 
 # %%
@@ -138,14 +219,16 @@ def lib_plot(
     x_dot_hat: Float1D,
     funcs_theta: Float2D,
     ylims: Optional[tuple[float, float]] = None,
-    **q_props: Any
+    **q_props: Any,
 ) -> None:
     """Create plot of function library vectors and smoothed derivative vectors."""
-    ax = plt.figure().add_axes((0,0,1,1))
+    ax = plt.figure().add_axes((0, 0, 1, 1))
     ax.plot(t, x_hat, ".", color=CEST, label="Kalman")
     ax.quiver(t, x_hat, dt, x_dot_hat * dt, color=CEST, **q_props)
     for i, func in enumerate(funcs_theta):
-        ax.quiver(t, x_hat, dt, func * dt, color=CMAP[i+3], label=f"$θ_{i}$", **q_props)
+        ax.quiver(
+            t, x_hat, dt, func * dt, color=CMAP[i + 3], label=f"$θ_{i}$", **q_props
+        )
     if ylims is not None:
         ax.set_ylim(*ylims)
     ax.legend()
@@ -155,38 +238,16 @@ def lib_plot(
 def shared_ylim(*args: Float1D) -> tuple[float, float]:
     data_lim = (cast(float, np.min(args)), cast(float, np.max(args)))
     diff = data_lim[1] - data_lim[0]
-    margin = diff * .1
+    margin = diff * 0.1
     return data_lim[0] - margin, data_lim[1] + margin
 
-# %%
-if __name__ == "__main__":
-    ylims = shared_ylim(x[zoom_inds], z[zoom_inds], x_hat[zoom_inds])
-    data_plot(t[zoom_inds], x[zoom_inds], z[zoom_inds], ylims=ylims)
-    funcs_theta = np.stack((np.ones_like(x_hat), x_hat, x_hat**2), axis=0)
-    smoothing_plot(
-        t[zoom_inds],
-        z[zoom_inds],
-        zd[:, zoom_inds],
-        x_hat[zoom_inds],
-        x_dot_hat[zoom_inds],
-        ylims=ylims,
-        **q_props
-    )
-    lib_plot(
-        t[zoom_inds],
-        fd[0, zoom_inds],
-        x_hat[zoom_inds],
-        x_dot_hat[zoom_inds],
-        funcs_theta[:, zoom_inds],
-        ylims=ylims,
-        **q_props
-    )
-
 
 # %%
 if __name__ == "__main__":
-    
-    model = ps.STLSQ(unbias=True).fit(funcs_theta.T, x_dot_hat)
-    print(model.coef_)
-
-# %%
+    m_conf = MathConfig({"ode_type": "sin", "dt": 1, "t_end": 2})
+    zoom_start = 0
+    zoom_end = zoom_start + 8
+    zoom_inds = slice(zoom_start, zoom_end, 1)
+    q_props = {"headwidth": 1.5, "headlength": 2, "angles": "xy"}
+    p_conf = PlotConfig({"zoom_inds": zoom_inds, "q_props": q_props})
+    run_example(m_conf, p_conf)
