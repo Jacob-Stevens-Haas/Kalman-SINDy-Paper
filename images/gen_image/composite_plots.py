e222291a694ac8a200fdbfa5b209ee8370438e87
Yash Bhangale <bhangaleys@gmail.com>
2024-03-04 19:20:49 -0800

    ENH: Summary plots now include kalman auto hyperparameter

--- images/gen_image/composite_plots.py
+++ images/gen_image/composite_plots.py
@@ -9,13 +9,13 @@ import ksindy_figs.plotting as plots
 from ksindy_figs.plotting import ExpKey
 
 exp_hexes = {
-    "Cubic HO": ExpKey("d41b48"),
-    "Duffing": ExpKey("9a2339"),
-    "Hopf": ExpKey("e6e691"),
-    "Lotka-Volterra": ExpKey("d453b9"),
-    "Rossler": ExpKey("831dc2"),
-    "SHO": ExpKey("a1757b"),
-    "Van der Pol": ExpKey("25e6c3"),
+    "Cubic HO": ExpKey("0b044c"),
+    "Duffing": ExpKey("b78632"),
+    "Hopf": ExpKey("2255b2"),
+    "Lotka-Volterra": ExpKey("128ce1"),
+    "Rossler": ExpKey("7a3950"),
+    "SHO": ExpKey("04b738"),
+    "Van der Pol": ExpKey("1a23e7"),
 }
 
 # %%
@@ -66,21 +66,24 @@ pass
 
 noise_params = {"sim_params.t_end": 16, "sim_params.noise_abs": 1}
 params_kalman = noise_params | {"diff_params.kind": "kalman"}
+params_kalmanauto = noise_params | {"diff_params.kind": "kalman"}
 params_tv = noise_params | {"diff_params.kind": "trend_filtered"}
 params_savgol = noise_params | {"diff_params.diffcls": "SmoothedFiniteDifference"}
 
+if params_kalmanauto==params_kalman:
+    raise ValueError("Cannot distinguish between kalman and kalman auto hyperparameters.")
 # %%
 fig = plots.plot_summary_test_train(
     [*exp_hexes.items()],
-    [("Kalman", params_kalman), ("TV", params_tv), ("SavGol", params_savgol)],
+    [("Kalman", params_kalman), ("KalmanAuto", params_kalmanauto), ("TV", params_tv), ("SavGol", params_savgol)],
     style="training",
-    row_cat="params",
+    row_cat="params",   
 )
 pass
 # %%
 fig = plots.plot_summary_test_train(
     [*exp_hexes.items()],
-    [("Kalman", params_kalman), ("TV", params_tv), ("SavGol", params_savgol)],
+    [("Kalman", params_kalman), ("KalmanAuto", params_kalmanauto), ("TV", params_tv), ("SavGol", params_savgol)],
     style="test",
     row_cat="params",
 )
@@ -91,41 +94,41 @@ pass
 # ### Additional code to demonstrate helper functions
 
 # %%
-plots.plot_point_across_experiments(
-    ("Kalman", params_kalman),
-    ...,
-    *exp_hexes.items(),
-    style="test",
-    shape=(1, 7),
-    annotations=False,
-)
+# plots.plot_point_across_experiments(
+#     ("Kalman", params_kalman),
+#     ...,
+#     *exp_hexes.items(),
+#     style="test",
+#     shape=(1, 7),
+#     annotations=False,
+# )
 # %%
 
 
-results = mitosis.load_trial_data(exp_hexes["Cubic HO"], trials_folder=data.TRIAL_DATA)
+# results = mitosis.load_trial_data(exp_hexes["Cubic HO"], trials_folder=data.TRIAL_DATA)
 
-plots.plot_experiment_across_gridpoints(
-    ("Cubic HO", exp_hexes["Cubic HO"]),
-    ("Kalman", params_kalman),
-    ("TV", params_tv),
-    ("SavGol", params_savgol),
-    style="test",
-    shape=(3, 1),
-)
+# plots.plot_experiment_across_gridpoints(
+#     ("Cubic HO", exp_hexes["Cubic HO"]),
+#     ("Kalman", params_kalman),
+#     ("TV", params_tv),
+#     ("SavGol", params_savgol),
+#     style="test",
+#     shape=(3, 1),
+# )
 # %%
-plots.plot_experiment_across_gridpoints(
-    ("Rossler", exp_hexes["Rossler"]),
-    ("Kalman", params_kalman),
-    ("TV", params_tv),
-    ("SavGol", params_savgol),
-    style="test",
-    shape=(3, 1),
-)
+# plots.plot_experiment_across_gridpoints(
+#     ("Rossler", exp_hexes["Rossler"]),
+#     ("Kalman", params_kalman),
+#     ("TV", params_tv),
+#     ("SavGol", params_savgol),
+#     style="test",
+#     shape=(3, 1),
+# )
 
 # %%
-from gen_experiments.odes import plot_ode_panel
+# from gen_experiments.odes import plot_ode_panel
 
-single_result = strict_find_grid_match(results, params=params_kalman)
-plot_ode_panel(cast(FullSINDyTrialData, single_result))
+# single_result = strict_find_grid_match(results, params=params_kalman)
+# plot_ode_panel(cast(FullSINDyTrialData, single_result))
 
 # %%
